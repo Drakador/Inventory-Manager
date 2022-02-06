0cc30ded8511bc3abd81f202d315fae19406bf51
Joshua Belfor <jebelfor@gmail.com>
2022-02-05 22:04:49 -0600

    Spring Starter files

---
+++ src/test/java/com/drakador/inventorymanager/inventorymanager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drakador.inventorymanager.inventorymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
