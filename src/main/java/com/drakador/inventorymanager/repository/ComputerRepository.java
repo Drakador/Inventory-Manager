3da7350dc69ad0f0c2b1e82f9a087cd6a365a369
Joshua <jebelfor@gmail.com>
2022-07-18 10:53:50 -0500

    Removed unused import

--- src/main/java/com/drakador/inventorymanager/repository/ComputerRepository.java
+++ src/main/java/com/drakador/inventorymanager/repository/ComputerRepository.java
@@ -1,6 +1,5 @@
 package com.drakador.inventorymanager.repository;
 
-import com.drakador.inventorymanager.model.bo.ComputerBo;
 import com.drakador.inventorymanager.model.dto.ComputerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
